6d290b23d0f592b9fbb3a051fb5f1b656ae5f05c
philkau <elsevierphil@gmail.com>
2020-10-15 22:16:33 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
+    <div>{process.env.NEXT_PUBLIC_MONGO_URL}</div>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
